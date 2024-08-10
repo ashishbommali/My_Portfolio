078a95f8becc96b8634a98e29e022cee79374d60
Ashish Bommali <ashishbommali@gmail.com>
2024-08-09 23:33:11 -0500

    emaijs added to the contact form

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,35 +1,47 @@
 // src/Pages/Contact.js
-import React from 'react';
+import React, { useRef } from 'react';
 import '../styles/pages/contact.css';
 import {
   FaEnvelope, FaLinkedin, FaGithub, FaPhone, FaLandmark
 } from 'react-icons/fa';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,  
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,  
+        form.current,                              
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY   
+      )
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          console.log('Message sent');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        }
+      );
+  };
+
   return (
     <div id="contact">
       <div className="contact-container">
         <div className="contact-form">
           <h2>Write me a Message</h2>
-          <form>
-
-            <input type="text" id="name" placeholder='Name' name="name" required />
-
-
-            <input type="tel" id="phone" placeholder='Phone' name="phone" required />
-
-
-            <input type="email" id="email" placeholder='Email' name="email" required />
-
-
-            <input type="text" id="company" placeholder='Company Name' name="company" required />
-
-
-            <input type="text" id="address" placeholder='Address' name="address" required />
-
-            <textarea id="message" placeholder='Message' name="message" required></textarea>
-
-            <button type="submit">Send</button>
+          <form ref={form} onSubmit={sendEmail}>
+            <input type="text" placeholder='Name' name="user_name" required />
+            <input type="tel" id="phone" placeholder='Phone' name="user_phone" required />
+            <input type="email" placeholder='Email' name="user_email" required />
+            <input type="text" id="company" placeholder='Company Name' name="user_company" required />
+            <textarea placeholder='Message' name="message" required></textarea>
+            <input type="submit" value="Send" />
           </form>
         </div>
         <div className="contact-details">
